c6c162899c644dba2d4c979c770ce55f1eb37df3
Theo Crandall <thrandale@gmail.com>
2023-08-11 19:47:19 -0600

    fixed build issues

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-/**
- * @format
- */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
+import ThemeWrapper from './src/ThemeWrapper';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <ThemeWrapper>
+      <App />
+    </ThemeWrapper>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
