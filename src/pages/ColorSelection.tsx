973e0c94a8086ef2671ddde36bbd79c530a2252a
Theo Crandall <thrandale@gmail.com>
2023-11-04 16:27:37 -0600

    Connect the color selector to BT

--- src/pages/ColorSelection.tsx
+++ src/pages/ColorSelection.tsx
@@ -4,25 +4,26 @@ import {StyleSheet, View} from 'react-native';
 import HeaderButtons from '../components/HeaderButtons';
 import {NavigationHelpers} from '@react-navigation/native';
 import {useTheme} from 'react-native-paper';
+import BTController, {Zone} from '../controllers/BTController';
 
 function ColorSelection(props: {navigation: NavigationHelpers<any, any>}) {
-  const theme = useTheme();
-
   const styles = StyleSheet.create({
     container: {
       display: 'flex',
       flex: 1,
-      backgroundColor: theme.colors.background,
       justifyContent: 'center',
       alignItems: 'center',
     },
   });
+
   return (
     <View style={styles.container}>
       <HeaderButtons hideBluetooth hideSettings navigation={props.navigation} />
       <ColorPicker
-        onColorChange={() => {
-          console.log('Color Changed');
+        onColorChange={color => {
+          BTController.getInstance().sendSetColorCommand(Zone.FRONT, color);
+          BTController.getInstance().sendSetColorCommand(Zone.MIDDLE, color);
+          BTController.getInstance().sendSetColorCommand(Zone.BACK, color);
         }}
       />
     </View>
